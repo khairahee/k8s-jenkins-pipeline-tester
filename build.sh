ee82290afb2dd825a35c97ead6f98635e8bd97da
Alan Platt <aplatt@equalexperts.com>
2018-05-21 16:55:43 +0100

    fix build command

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 check_dependency() {
   local DEPENDANCY="${1}"
   command -v "${DEPENDANCY}" >/dev/null 2>&1 || err "${DEPENDANCY} is required but not installed.  Aborting."
@@ -20,7 +22,7 @@ main () {
   export REPO="microdc"
   export VERSION="${VERSION-latest}"
   log "creating local build"
-  docker build --rm -t "${REPO}/${APP}:${VERSION}" -t "${REPO}/${APP}:latest".
+  docker build --rm -t "${REPO}/${APP}:${VERSION}" -t "${REPO}/${APP}:latest" .
   log "done!"
 }
 
